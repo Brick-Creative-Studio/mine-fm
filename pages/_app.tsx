36a0fa94eb5d3ca1d98d2ed6da942d2b3e0ab826
Bobby <bobby.bah@gmail.com>
2022-10-14 20:11:54 -0400

    update file structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { WagmiConfig, chain, configureChains, createClient } from 'wagmi'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { publicProvider } from 'wagmi/providers/public'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+const { chains, provider } = configureChains(
+  [
+    // chain.mainnet,
+    // chain.polygon,
+    // chain.optimism,
+    // chain.arbitrum,
+    // chain.ropsten,
+    chain.goerli,
+  ],
+  [
+    jsonRpcProvider({
+      priority: 0,
+      rpc: () => ({
+        http: process?.env?.ETH_RPC_URL || '',
+      }),
+    }),
+    publicProvider({ priority: 1 }),
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Zounds forms',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
+
+return( 
+
+<Component {...pageProps} />
+)
 }
 
 export default MyApp
