f8960c96cb9f8869ded3e5fda251379056e3037f
Bobby <bobby.bah@gmail.com>
2022-10-14 22:03:24 -0400

    Set up chain connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,42 +6,45 @@ import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { publicProvider } from 'wagmi/providers/public'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
-const { chains, provider } = configureChains(
-  [
-    // chain.mainnet,
-    // chain.polygon,
-    // chain.optimism,
-    // chain.arbitrum,
-    // chain.ropsten,
-    chain.goerli,
-  ],
-  [
-    jsonRpcProvider({
-      priority: 0,
-      rpc: () => ({
-        http: process?.env?.ETH_RPC_URL || '',
+
+  const { chains, provider } = configureChains(
+    [
+      // chain.mainnet,
+      // chain.polygon,
+      // chain.optimism,
+      // chain.arbitrum,
+      // chain.ropsten,
+      chain.goerli,
+    ],
+    [
+      jsonRpcProvider({
+        priority: 0,
+        rpc: () => ({
+          http: process?.env?.ETH_RPC_URL || '',
+        }),
       }),
-    }),
-    publicProvider({ priority: 1 }),
-  ]
-)
+      publicProvider({ priority: 1 }),
+    ]
+  )
 
-const { connectors } = getDefaultWallets({
-  appName: 'Zounds forms',
-  chains,
-})
+  const { connectors } = getDefaultWallets({
+    appName: 'Mine.fm',
+    chains,
+  })
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-})
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  })
 
-return( 
-
-<Component {...pageProps} />
-)
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} initialChain={chain.goerli}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+      return (
+        <Html>
+          <Head />
+            <Main />
+            <NextScript />
+        </Html>
+      )
+    }
+  }
+  
