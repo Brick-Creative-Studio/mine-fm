9f1f2df04d38d44a5178f46874ea3c3a0f007694
Bobby <bobby.bah@gmail.com>
2023-04-08 10:57:36 -0400

    enable linter

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+
   },
 
   webpack(config) {
