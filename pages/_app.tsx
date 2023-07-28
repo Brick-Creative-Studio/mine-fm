99a07d76b813866dc9e473ce6a762e89b84da0b7
Bobby <bobby.bah@gmail.com>
2023-07-27 20:02:12 -0400

    update rainbowkit and wagmi config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,47 +2,66 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@rainbow-me/rainbowkit/styles.css'
 import { Analytics } from '@vercel/analytics/react'
-import { RainbowKitProvider, getDefaultWallets, connectorsForWallets } from '@rainbow-me/rainbowkit'
-import { WagmiConfig, configureChains, createClient } from 'wagmi'
-import {mainnet, goerli, optimism} from 'wagmi/chains'
-import Layout  from '../components/Layout/Layout'
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+} from '@rainbow-me/rainbowkit'
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { mainnet, goerli, optimism } from 'wagmi/chains'
+import Layout from '../components/Layout/Layout'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { argentWallet, trustWallet, ledgerWallet } from '@rainbow-me/rainbowkit/wallets'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-
+import { publicProvider } from 'wagmi/providers/public'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const { chains, provider } = configureChains(
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [optimism, goerli],
     [
-      // mainnet,
-      // chain.polygon,
-      optimism
-      // chain.arbitrum,
-      // goerli,
+      alchemyProvider({
+        apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID
+          ? process.env.NEXT_PUBLIC_ALCHEMY_ID
+          : '',
+      }),
     ],
-    [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID ? process.env.NEXT_PUBLIC_ALCHEMY_ID : ""})
-    ]
-  )
 
-  const { connectors } = getDefaultWallets({
+  )
+  const projectId = '4fe03bd3764f7528d95b9ebed65f5035'
+
+  const { wallets } = getDefaultWallets({
     appName: 'MINE.FM',
-    chains
+    projectId,
+    chains,
   })
 
-  const wagmiClient = createClient({
+  const connectors =connectorsForWallets([
+      ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [
+        argentWallet({ projectId, chains }),
+        trustWallet({ projectId, chains }),
+        ledgerWallet({ projectId, chains }),
+      ],
+    },
+  ])
+
+  const wagmiConfig = createConfig({
     autoConnect: true,
     connectors,
-    provider,
+    publicClient,
+    webSocketPublicClient,
+
   })
 
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains} initialChain={optimism}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <Analytics />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <Analytics />
       </RainbowKitProvider>
     </WagmiConfig>
   )
