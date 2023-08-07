afc37019daa7318f8887231fc815e64d50f72c89
extrawaterjoe <jlazarz87@gmail.com>
2023-08-07 14:57:17 -0400

    icons and gap

--- components/Sections/StreamInfo-Desktop.tsx
+++ components/Sections/StreamInfo-Desktop.tsx
@@ -9,30 +9,42 @@ const StreamInfoDesktop = () => {
 
   return (
     <div className={'flex flex-row px-4 overflow-scroll w-full bg-[#B999FA]'}>
-      <div className={'flex-row flex items-center justify-center w-full'}>
+      <div className={'flex-row gap-2 flex items-center justify-center w-full'}>
+        <div
+          className={
+            'flex-row flex items-center justify-center w-8 h-8 bg-[#1D0045] rounded-full'
+          }
+        />
+
         <p className={'text-[#1D0045]'}>{`${hostTitle}`}</p>
       </div>
 
-      <div className={'flex-row flex items-center justify-center w-full'}>
+      <div className={'flex-row gap-2 flex items-center justify-center w-full'}>
+        <Image width={24} height={24} src={'/UserIcon.svg'} alt="share button" />
         <p className={'text-[#1D0045]'}>{`${attendanceCount} attendees`}</p>
       </div>
 
-      <div className={'flex-row flex items-center justify-center w-full'}>
+      <div className={'flex-row gap-2 flex items-center justify-center w-full'}>
+        <Image width={24} height={24} src={'/ETHIcon.svg'} alt="share button" />
         <p className={'text-[#1D0045]'}>{`${entryFee} eth`}</p>
       </div>
 
-      <div className={'flex-row flex items-center justify-center w-full'}>
+      <div className={'flex-row gap-2 flex items-center justify-center w-full'}>
+        <Image width={24} height={24} src={'/CubeIcon.svg'} alt="share button" />
         <p className={'text-[#1D0045]'}>{`${treasury} eth`}</p>
       </div>
 
-      <div className={'flex-row flex items-center justify-center w-full'}>
+      <div className={'flex-row gap-2 flex items-center justify-center w-full'}>
+        <Image width={24} height={24} src={'/TimerIcon.svg'} alt="share button" />
         <p className={'mr-2 text-[#1D0045]'}>46:58</p>
       </div>
 
       <button
-        className={'flex-row flex items-center justify-center w-full bg-transparent'}
+        className={
+          'flex-row gap-2 flex items-center justify-center w-full bg-transparent'
+        }
       >
-        <Image width={24} height={24} src={'/share.svg'} alt="share button" />
+        <Image width={24} height={24} src={'/ShareIcon.svg'} alt="share button" />
         <p className={'text-[#1D0045] ml-2'}>Share</p>
       </button>
     </div>
