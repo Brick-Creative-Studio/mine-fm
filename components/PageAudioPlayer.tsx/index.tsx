3a5511623542e7b19363f111c8b3051c52601983
extrawaterjoe <jlazarz87@gmail.com>
2023-08-22 15:11:37 -0400

    check playable

--- components/PageAudioPlayer.tsx/index.tsx
+++ components/PageAudioPlayer.tsx/index.tsx
@@ -1,11 +1,32 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Image from 'next/image'
 
 const PageAudioPlayer = () => {
   const audioRef = useRef<HTMLAudioElement>(null)
-  const [isPlaying, setIsPlaying] = useState(false)
+  const [playable, setPlayable]: [null | boolean, Function] = useState(null)
+  const [isPlaying, setIsPlaying]: [boolean, Function] = useState(false)
+
+  // useEffect to check if audio src is playable
+  // will need a way to communicate from the server to the client
+  // when a stream goes live, and when it ends
+  // for when a user is on the page before the stream goes live
+  useEffect(() => {
+    const audio = audioRef.current?.currentSrc
+    fetch(audio as string)
+      .then((res) => {
+        if (res.ok) {
+          setPlayable(true)
+        } else {
+          setPlayable(false)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
   const handlePlayPause = () => {
+    if (!playable) return
     if (audioRef.current) {
       if (isPlaying) {
         audioRef.current.pause()
@@ -19,7 +40,7 @@ const PageAudioPlayer = () => {
 
   return (
     <>
-      {!isPlaying ? (
+      {!isPlaying && playable ? (
         <Image
           src="/PlayIcon.svg"
           alt=""
@@ -27,7 +48,7 @@ const PageAudioPlayer = () => {
           height={150}
           onClick={handlePlayPause}
         />
-      ) : (
+      ) : isPlaying && playable ? (
         <Image
           src="/PauseIcon.svg"
           alt=""
@@ -35,9 +56,12 @@ const PageAudioPlayer = () => {
           height={150}
           onClick={handlePlayPause}
         />
-      )}
+      ) : playable === false ? (
+        <div>Stream is not Live</div>
+      ) : null}
       <audio ref={audioRef}>
         <source src="https://stream-relay-geo.ntslive.net/stream/64.aac?client=NTSWebApp&t=1691770293785" />
+        {/* <source src="https://s1.evenings.co/s/mine.fm" /> */}
       </audio>
     </>
   )
