d5a98cd12d772438ac6d5a59a2af0f24994be773
Bobby <bobby.bah@gmail.com>
2023-09-14 12:23:04 -0400

    update create page UI

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,35 +1,50 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-
-
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function CreatePage() {
+  return (
+    <div className="flex flex-col justify-center items-center mt-24 h-full">
+      <div className={'flex flex-col w-full'}>
+        <p className={'my-1 ml-12 self-start text-[64px] text-[#B999FA]'}> CREATE </p>
+        <div className={'w-full border-2 border-solid border-[#B999FA]'} />
+      </div>
+      <div
+        className={
+          'h-full w-full flex flex-col md:flex-row justify-center md:justify-around items-center mt-8 md:mt-20 md:px-12'
+        }
+      >
+        <div className={'flex flex-col items-center'}>
+          <p className="text-[#B999FA] text-[48px] mb-4 md:mb-10"> LIVESTREAM </p>
 
-    return (
-        <div className="flex flex-col justify-center items-center mt-24">
-          <div className={'flex flex-col  items-center '}>
-            <h1 className={'my-1'}> Create a Moodscape </h1>
-            <p className="mb-16 mx-4 text-lg"> A token-gated event to enjoy music with a community and share your mood </p>
-          </div>
           <Link href={`/create/livestream`}>
-            <div className="flex flex-col justify-center items-center w-2/3 md:w-1/4  h-48 p-4 border-solid border-blue-500/50 rounded-lg bg-black/25 mb-12">
-                <h1 className="mt-4"> Livestream </h1>
-                <h3 className="-mt-4 text-center"> Audio livestreams events with reward distributions. Token gated and intimate. </h3>
-            </div >
+          <div className="flex flex-col justify-center items-center w-80 h-48 p-8 border-solid md:w-[35rem] border-[#B999FA] border-4 rounded-full bg-[#1D0045] mb-10 cursor-pointer hover:opacity-80">
+            <h3 className="text-center text-[20px] font-light text-[#B999FA]">
+              {' '}
+              Audio livestreams events with reward distributions. Token gated and
+              intimate.{' '}
+            </h3>
+          </div>
         </Link>
-
-
-           
-            <div className="flex flex-col justify-center items-center border-solid border-green-500/50 h-56 py-2 px-8 w-2/3 md:w-1/4 mb-12 rounded-lg bg-black/25  focus:ring">
-                <h1 className=""> IRL </h1>
-                <h3 className="text-center -mt-4"> Create a hybrid event for IRL and online. Contact team for private access </h3>
-                <a target="_blank" href="https://mine.fm/Stay-Connected" rel="noopener noreferrer">
-
-                <button className="rounded-lg bg-transparent cursor-pointer">Contact</button>
-                </a>
-               
-            </div>
         </div>
-    )
+        <div className={'flex flex-col items-center'}>
+        <p className="text-[#B999FA] text-[48px] mb-4 md:mb-10"> IRL </p>
+        <a
+          target="_blank"
+          href="https://mine.fm/Stay-Connected"
+          rel="noopener noreferrer"
+        >
+
+        <div className="flex flex-col justify-center items-center border-solid border-4 border-[#B999FA] h-48 p-8 w-80 md:w-[35rem] mb-10 rounded-full bg-[#1D0045] cursor-pointer hover:opacity-80">
+
+          <h3 className="text-center text-[20px] font-light text-[#B999FA]">
+            {' '}
+            Create a hybrid event for IRL and online. Contact team for private access{' '}
+          </h3>
+        </div>
+        </a>
+        </div>
+      </div>
+    </div>
+  )
 }
