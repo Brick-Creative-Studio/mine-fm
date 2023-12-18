1797dd0c18cf80a506500e727f372ff48eacc381
Kevin Gil <93kevingil@gmail.com>
2023-12-18 18:20:24 -0300

    custom connect wallet button in nav

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { NavBar, navActions, navCreate, navLogo, marquee } from "./styles.css";
+import React, { useEffect, useState } from 'react'
+import { NavBar, navActions, navCreate, navLogo, marquee } from './styles.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import NavMenu from './NavMenu'
 import { useIsMounted } from '../../hooks/useMounted'
 import { useAccount } from 'wagmi'
 import { useProfileStore } from 'stores'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+// import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { ConnectButton } from 'components/ConnectButton/ConnectButton'
 import { useLayoutStore } from 'stores'
-import useGetUser from "../../hooks/useGetUser";
-import { User } from "../../types/User";
-import { useRouter } from "next/router";
+import useGetUser from '../../hooks/useGetUser'
+import { User } from '../../types/User'
+import { useRouter } from 'next/router'
 
 const Nav = () => {
   const isMounted = useIsMounted()
   const { setSignerAddress, isMobile } = useLayoutStore((state) => state)
-  const [ isHidden, setHidden ] = useState<boolean>( false )
-  const { hasAccount, setAura, setIdentity, id, resetProfileState, setHasAccount } = useProfileStore((state) => state)
+  const [isHidden, setHidden] = useState<boolean>(false)
+  const { hasAccount, setAura, setIdentity, id, resetProfileState, setHasAccount } =
+    useProfileStore((state) => state)
   const router = useRouter()
 
   const { address } = useAccount({
@@ -33,7 +35,7 @@ const Nav = () => {
   const { error, isLoading, user } = useGetUser(address as string)
 
   useEffect(() => {
-    if (user){
+    if (user) {
       setIdentity({
         id: user.id,
         m_tag: user.miner_tag,
@@ -47,26 +49,24 @@ const Nav = () => {
         colorOne: user.colorOne!!,
         colorTwo: user.colorTwo!!,
         colorThree: user.colorThree!!,
-        direction: user.direction!!
+        direction: user.direction!!,
       })
       setHasAccount(true)
       return
     }
 
-    if(error || !user) {
+    if (error || !user) {
       resetProfileState()
       return
     }
-
   }, [user, error, isLoading])
 
   useEffect(() => {
-    if(router.pathname.includes('livestream')){
+    if (router.pathname.includes('livestream')) {
       setHidden(true)
       return
     }
     setHidden(false)
-
   }, [router])
 
   return isHidden ? null : isMounted && address ? (
@@ -82,36 +82,61 @@ const Nav = () => {
           />
         </Link>
       </div>
-      {
-        isMobile ? (
-          <div className={'flex w-fit h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative cursor-pointer md:ml-12'}>
-            <div className={'animate-marquee flex items-center justify-center pt-1 whitespace-nowrap'}>
-              <div className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'} />
-              <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
-            </div>
-            <div className={'animate-marquee2 absolute top-0 flex items-center whitespace-nowrap'}>
-              <div className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'} />
-              <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
-            </div>
+      {isMobile ? (
+        <div
+          className={
+            'flex w-fit h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative cursor-pointer md:ml-12'
+          }
+        >
+          <div
+            className={
+              'animate-marquee flex items-center justify-center pt-1 whitespace-nowrap'
+            }
+          >
+            <div
+              className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'}
+            />
+            <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
           </div>
-        ) : (
-          <div className={'flex items-center justify-center w-2/5 h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative cursor-pointer md:ml-16'}>
-            <div className={'flex items-center justify-center pt-1 whitespace-nowrap'}>
-              <div className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'} />
-              <p className={'text-[#B999FA] text-center text-[16px]'}> MINE.FM Live Broadcasts </p>
-            </div>
-
+          <div
+            className={
+              'animate-marquee2 absolute top-0 flex items-center whitespace-nowrap'
+            }
+          >
+            <div
+              className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'}
+            />
+            <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
           </div>
-        )
-      }
+        </div>
+      ) : (
+        <div
+          className={
+            'flex items-center justify-center w-2/5 h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative cursor-pointer md:ml-16'
+          }
+        >
+          <div className={'flex items-center justify-center pt-1 whitespace-nowrap'}>
+            <div
+              className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'}
+            />
+            <p className={'text-[#B999FA] text-center text-[16px]'}>
+              {' '}
+              MINE.FM Live Broadcasts{' '}
+            </p>
+          </div>
+        </div>
+      )}
 
       <div className={navActions}>
-        <Link
-          key={'about-us'}
-          href={'/about-us'}
-        >
-        <p className={`m-2 text-xl text-[#B999FA] cursor-pointer hover:text-purple-800 ${isMobile ? 'hidden' : null}`}> ABOUT US </p>
-
+        <Link key={'about-us'} href={'/about-us'}>
+          <p
+            className={`m-2 text-xl text-[#B999FA] cursor-pointer hover:text-purple-800 ${
+              isMobile ? 'hidden' : null
+            }`}
+          >
+            {' '}
+            ABOUT US{' '}
+          </p>
         </Link>
         {address && <NavMenu hasAccount={hasAccount} signerAddress={address} />}
       </div>
@@ -132,37 +157,63 @@ const Nav = () => {
       {/*<div className={marquee}>*/}
       {/*  <h3 className={'float-left'}> MINE.FM Radio </h3>*/}
       {/*</div>*/}
-      {
-        isMobile ? (
-          <div className={'flex w-fit h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative cursor-pointer md:ml-12'}>
-            <div className={'animate-marquee flex items-center justify-center pt-1 whitespace-nowrap'}>
-              <div className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'} />
-              <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
-            </div>
-            <div className={'animate-marquee2 absolute top-0 flex items-center whitespace-nowrap'}>
-              <div className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'} />
-              <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
-            </div>
+      {isMobile ? (
+        <div
+          className={
+            'flex w-fit h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative cursor-pointer md:ml-12'
+          }
+        >
+          <div
+            className={
+              'animate-marquee flex items-center justify-center pt-1 whitespace-nowrap'
+            }
+          >
+            <div
+              className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'}
+            />
+            <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
           </div>
-        ) : (
-          <div className={'flex items-center justify-center w-2/5 h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative md:left-12 lg:left-28 cursor-pointer mx-auto'}>
-            <div className={'flex items-center justify-center pt-1 whitespace-nowrap'}>
-              <div className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'} />
-              <p className={'text-[#B999FA] text-center text-[16px]'}> MINE.FM Live Broadcasts </p>
-            </div>
-
+          <div
+            className={
+              'animate-marquee2 absolute top-0 flex items-center whitespace-nowrap'
+            }
+          >
+            <div
+              className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'}
+            />
+            <p className={'text-[#B999FA] text-[16px]'}> MINE.FM Live Broadcasts </p>
           </div>
-        )
-      }
+        </div>
+      ) : (
+        <div
+          className={
+            'flex items-center justify-center w-2/5 h-12 border-solid border-[#654A8F] rounded-md overflow-x-hidden overflow-y-hidden relative md:left-12 lg:left-28 cursor-pointer mx-auto'
+          }
+        >
+          <div className={'flex items-center justify-center pt-1 whitespace-nowrap'}>
+            <div
+              className={'rounded-full w-2 h-2 mx-2 bg-red-700 animate-pulse float-left'}
+            />
+            <p className={'text-[#B999FA] text-center text-[16px]'}>
+              {' '}
+              MINE.FM Live Broadcasts{' '}
+            </p>
+          </div>
+        </div>
+      )}
 
       <div className={navActions}>
-        <Link
-          key={'about-us'}
-          href={'/about-us'}
-        >
-          <p className={`m-2 text-xl text-[#B999FA] cursor-pointer hover:text-purple-800 ${isMobile ? 'hidden' : null}`}> ABOUT US </p>
-
-        </Link>        <ConnectButton />
+        <Link key={'about-us'} href={'/about-us'}>
+          <p
+            className={`m-2 text-xl text-[#B999FA] cursor-pointer hover:text-purple-800 ${
+              isMobile ? 'hidden' : null
+            }`}
+          >
+            {' '}
+            ABOUT US{' '}
+          </p>
+        </Link>{' '}
+        <ConnectButton />
       </div>
     </div>
   )
