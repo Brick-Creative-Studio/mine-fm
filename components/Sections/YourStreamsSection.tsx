1e2965bf2415d793e6cac0e963e542fafcc59fce
Bobby <bobby.bah@gmail.com>
2024-01-18 16:00:08 -0500

    fix loop

--- components/Sections/YourStreamsSection.tsx
+++ components/Sections/YourStreamsSection.tsx
@@ -15,23 +15,29 @@ export default function YourStreamsSection({ aura, userId }: Props) {
     console.log('USER ID', userId)
     const fetchUserRsvps = async () => {
       const userRSVPs: Rsvp[] = await getRSVPsWhere(userId)
+      console.log('check rsvps', userRSVPs)
       if (userRSVPs) {
         const array: string[] = []
-        userRSVPs.forEach(async (rsvp: Rsvp) => {
-          const userEventsReq = await getEventsWhere({ id: rsvp.eventID })
+
+        for(let i = 0; i < userRSVPs.length; i++){
+          const userEventsReq = await getEventsWhere({ id: userRSVPs[i].eventID })
           if (userEventsReq) {
             const memoryCard = userEventsReq[0].memoryCard
             array.push(memoryCard!)
           }
-        })
+        }
+        // userRSVPs.forEach(async (rsvp: Rsvp) => {
+        //
+        // })
         setMemoryCards(array)
       }
     }
     fetchUserRsvps()
   }, [userId])
+  {console.log('MEMORY CARDS', memoryCards)}
+
   return memoryCards ? (
     <div className="flex items-center justify-start h-96 w-[100vw]">
-      {console.log('MEMORY CARDS', memoryCards)}
       {memoryCards.map((card) => {
         return (
           <div className="w-[300px] aspect-square relative" key={card}>
